05e32c12213c25bd6fad56af4c40ce17a3d1130b
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-18 11:25:05 -0500

    Uploading getenv

---
+++ getenv.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char **words(char *line)
+{
+	char *sep = "=";
+	char **tokens = malloc(sizeof(char *) * 64);
+	char *word;
+	int i = 0;
+
+	word = strtok(line, sep);
+	while (word != NULL)
+	{
+		tokens[i] = word;
+		i++;
+		word = strtok(NULL, sep);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
+
+char *_getenv(char *name)
+{
+	extern char **environ;
+	char **token;
+	int i = 0;
+
+	token = words(*environ);
+	while (*(token + i))
+	{
+		if (*(token + i) == name)
+		{
+			i++;
+			break;
+		}
+		else if (*(token + i) == NULL)
+		return(NULL);
+		else
+		{
+			i++;
+		}
+	}
+	return(*(token + i));
+}
+
+int main (void)
+{
+	char *value;
+
+	value = getenv("PWD");
+	printf("El valor de HOME es = %s\n", value);
+	return(0);
+}
