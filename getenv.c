5908e33de9219ad94e566ecd63fcf38e6ae792e5
danielcinome <danielchinome987@gmail.com>
2019-11-19 14:28:31 -0500

    getenv

--- getenv.c
+++ getenv.c
@@ -1,69 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "functions.h"
 
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
 
-	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
-	{
-		i++;
-	}
-	return (s1[i] - s2[i]);
-}
-
-char **words(char *line, char *sep)
-{
-	char **tokens = malloc(sizeof(char *) * 1024);
-	char *word;
-	int i = 0;
-
-	word = strtok(line, sep);
-	while (word != NULL)
-	{
-		tokens[i] = word;
-		i++;
-		word = strtok(NULL, sep);
-	}
-	tokens[i] = NULL;
-	return (tokens);
-}
-
-char *_getenv(char *name)
-{
-	extern char **environ;
-	char **save = environ;
-	char **tokenizado;
-	int i = 0;
-
-	while (environ[i] != NULL)
-	{
-        	save[i] = environ[i];
-        	i++;
-		
-    	}
-	save[i] = NULL;
-	i = 0;
-	while(save[i] != NULL)
-	{
-		tokenizado = words(*(save + i), "=");
-		printf("%s = %s\n",tokenizado[0], tokenizado[1]);
-		if (_strcmp(tokenizado[0], name) == 0)
-		{
-			break;
-		}
-	i++;
-	}
-	return(tokenizado[1]);
-}
-
-int main (void)
+int main(int argc, char *argv[], char **env)
 {
 	char *value;
 
-	value = _getenv("PATH");
-	printf("El valor de PATH es %s\n", value);
-	return(0);
+	value = _getenv(argv[1], env);
+	printf("El valor de %s es %s\n", argv[1], value);
+	return (0);
 }
