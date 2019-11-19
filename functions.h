45abfce806b955559e806933156428dfbce02cbc
danielcinome <danielchinome987@gmail.com>
2019-11-19 18:45:08 -0500

    add libreria and prototype

--- functions.h
+++ functions.h
@@ -1,9 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 typedef struct token_s
 {
