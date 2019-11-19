a51bfa79153214896e754179eb38a70ee81afc31
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-19 14:25:11 -0500

    Adding betty stle

--- function_getenv.c
+++ function_getenv.c
@@ -8,19 +8,19 @@ char *_getenv(char *name, char **env)
 
 	while (env[i] != NULL)
 	{
-			save[i] = env[i];
-			i++;
+		save[i] = env[i];
+		i++;
 	}
 	save[i] = NULL;
 	i = 0;
-	while(save[i] != NULL)
+	while (save[i] != NULL)
 	{
 		tokenizado = words(*(save + i), "=");
 		/*printf("%s = %s\n",tokenizado[0], tokenizado[1]);*/
-		if(_strcmp(tokenizado[0], name) == 0)
-		{
-			break;
-		}
+			if (_strcmp(tokenizado[0], name) == 0)
+			{
+				break;
+			}
 		i++;
 	}
 	return (tokenizado[1]);
