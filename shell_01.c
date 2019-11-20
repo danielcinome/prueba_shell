b3ae0a445a46d00df26b8d8b1fa25b8c985f4547
danielcinome <danielchinome987@gmail.com>
2019-11-20 17:40:23 -0500

    correct enter

--- shell_01.c
+++ shell_01.c
@@ -6,23 +6,25 @@ int main(int argc, char *argv[])
 	char **tokenizado;
 	int status, val_fd = 0;
 	pid_t hijo;
-		
-	while(1)
+
+	while (1)
 	{
 		hijo = fork();
 		if (hijo == -1)
 		{
 			perror("Error: hijo");
-			return(-1);
+			return (-1);
 		}
 		val_fd = isatty(STDIN_FILENO);
 		if (hijo == 0)
 		{
 			if (argc == 1)
 			{
-			write(1, "#cisfun$ ", 10);
-			cont = read_line();
-			tokenizado = words(cont, " \n\a\b\r\t\0");
+				write(1, "#cisfun$ ", 10);
+				cont = read_line();
+			if (*cont == '\n' || (*cont == -1)) /* enter */
+				return (1);
+				tokenizado = words(cont, " \n\a\b\r\t\0");
 				if (execve(tokenizado[0], tokenizado, NULL) == -1)
 				{
 					perror(argv[0]);
@@ -37,7 +39,7 @@ int main(int argc, char *argv[])
 			wait(&status);
 			if (val_fd == 0)
 			{
-			write(1, "#cisfun$ ", 10);
+				write(1, "#cisfun$ ", 10);
 				break;
 			}
 		}
